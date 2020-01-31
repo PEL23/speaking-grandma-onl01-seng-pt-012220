da8b648fb77ee38eaa3b61c2006161c4589e6fac
excellent-validator-4322 <plarmony24@gmail.com>
2020-01-31 04:14:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,8 @@ def speak_to_grandma(phrase)
   if 
   elsif phrase == "I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
-     
+     phrase == phrase.upcase
+     "NO, NOT SINCE 1938!"
   else 
      "HUH?! SPEAK UP, SONNY!"
   end
